f6e125403d5b20bf30406408ecfd75ad2dbaed1a
Mohanad <mohaned1929@gmail.com>
2022-01-26 19:56:37 -0500

    the red car is ready for the trial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
     public static void main(String [] args){
         CarBasics car=new CarBasics();
-        
+        car.color="red";
+        car.engine="7999cc";
+        car.number="119018";
+        System.out.println(car.color);
     }
 }
