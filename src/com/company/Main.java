518fe9b258f808b70f46c0dd75380cdff7ada04d
Mohanad <mohaned1929@gmail.com>
2022-01-26 20:56:59 -0500

    here are the changes that you asked me to make please make up your mind

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,11 @@ package com.company;
 public class Main {
     public static void main(String [] args){
         CarBasics car=new CarBasics();
-<<<<<<< Updated upstream
-        car.color="red";
-        car.engine="7999cc";
-        car.number="119018";
-=======
         car.color="Green";
         car.number="789";
         car.engine="54444cc";
         car.door="3";
->>>>>>> Stashed changes
+
         System.out.println(car.color);
     }
 }
